7a384e8a133e2d1576ffb6260f3e9cdd03a91184
auiri4 <bianca.livonius@auiricom.br>
2026-02-13 18:30:47 -0300

    config: astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: 'server',
-  integrations: [react()],
+  output: "server",
+  adapter: vercel(),
 });
