a363ae1ace597fbd373d6a774888267e3cdbe474
auiri4 <bianca.livonius@auiricom.br>
2026-02-13 22:57:46 -0300

    chore: set site URL to abm.auiri.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://abm.auiri.com.br",
   output: "server",
   adapter: vercel(),
   integrations: [react()],
